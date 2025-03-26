e8e6095248050cab72764a52420ea02ca9e30ef6
Elangsrya <elangkajah@gmail.com>
2025-03-26 20:39:54 +0700

    update style web

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 //Ini Javascript External
+function showSection(sectionId){
+    document.getElementById('persegi').style.display = 'none';
+    document.getElementById('persegi-panjang').style.display = 'none';
+
+    document.getElementById(sectionId).style.display = 'block';
+}
+
+window.onload = function() {
+    showSection('persegi');
+};
